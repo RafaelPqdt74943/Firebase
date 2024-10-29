b630ed8a9bd30b880052b642dee8068420e61205
RAFAEL SANTOS <sgtrafaelsantos0801@gmail.com>
2024-10-29 12:25:06 -0300

    primeiro commit firebase

---
+++ app.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+            apiKey: "AIzaSyD_ToyvUXvhw8DAKO_y9BHawKuDEmNSNCs",
+            authDomain: "escola-c21d0.firebaseapp.com",
+            projectId: "escola-c21d0",
+            storageBucket: "escola-c21d0.appspot.com",
+            messagingSenderId: "895380866713",
+            appId: "1:895380866713:web:a66e92eefd780691db1a08",
+            measurementId: "G-PBHT5BBTKP"
+          };
